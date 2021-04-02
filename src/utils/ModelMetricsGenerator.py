cda253ffcd8c1eade00c0c3ed953e6f32ac626a3
Gustavo A <37037671+gusAGR@users.noreply.github.com>
2021-04-02 16:48:45 +0200

    ModelMetricGenerartor added

---
+++ src/utils/ModelMetricsGenerator.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, fbeta_score
+import matplotlib.pyplot as plt
+import time
+
+
+class ModelMetricsGenerator:
+
+    def __init__(self, test_truth):
+        self._test_truth = np.asarray(test_truth)
+        self._predicted = 0
+        self._fit_time = 0
+        self._predict_time = 0
+
+    def generate_metrics(self, model, train_data, train_truth, test_data):
+        fit_start_time = time.time()
+        model.fit(train_data, train_truth)
+        self._fit_time = time.time() - fit_start_time
+
+        predict_start_time = time.time()
+        self._predicted = np.asarray(model.predict(test_data))
+        self._predict_time = time.time() - predict_start_time
+
+        return model
+
+    def print_results(self):
+        self.print_metrics()
+        #self.print_confusion_matrix()
+
+    def print_metrics(self):
+        self.print_accuracy()
+        self.print_precision()
+        self.print_recall()
+        self.print_specifity()
+        self.print_f2_score()
+        self.print_fit_time()
+        self.print_predict_time()
+
+    def print_accuracy(self):
+        #print(self._test_truth.shape)
+        #print(self._predicted.shape)
+        accuracy = accuracy_score(self._test_truth, self._predicted, normalize=True)
+        print(f'Accuracy: {accuracy}')
+
+    def print_precision(self):
+        precision = precision_score(self._test_truth, self._predicted)
+        print(f'Precision: {precision}')
+
+    def print_recall(self):
+        recall = recall_score(self._test_truth, self._predicted)
+        print(f'Recall: {recall}')
+
+    def print_specifity(self):
+        recall = recall_score(self._test_truth, self._predicted, pos_label=0)
+        print(f'Specifity: {recall}')
+
+    def print_f2_score(self):
+        f2_score = fbeta_score(self._test_truth, self._predicted, beta=2)
+        print(f'F2-score: {f2_score}')
+
+    def print_fit_time(self):
+        fit_time_in_seconds = round(self._fit_time, 6)
+        print(f'Fit time: {fit_time_in_seconds} seconds.')
+
+    def print_predict_time(self):
+        predict_time_in_seconds = round(self._predict_time, 6)
+        print(f'Predict time: {predict_time_in_seconds} seconds.')
+
+    def print_confusion_matrix(self):
+        print(f'Confusion matrix:\n {confusion_matrix(self._test_truth, self._predicted)}')
---
+++ test/utils/ModelMetricsGeneratorTest.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest import TestCase
+import pandas as pd
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+from src.utils.ModelMetricsGenerator import ModelMetricsGenerator
+
+
+class TestModelMetricsGenerator(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_output(self):
+        train_data = pd.DataFrame({'feature1': [1, 2, 3], 'feature2': [4, 5, 6]})
+        train_truth = pd.DataFrame({'truth': [0, 1, 0]})
+
+        test_data =  pd.DataFrame({'feature1': [1, 4, 3], 'feature2': [4, 9, 6]})
+        test_truth = pd.DataFrame({'truth': [0, 1, 0]})
+
+        model = svm.LinearSVC()
+
+        metric_generator = ModelMetricsGenerator(test_truth)
+        metric_generator.generate_metrics(model, train_data, train_truth, test_data)
+        metric_generator.print_results()
+
+    def test_output_with_real_dataset(self):
+        input_data = pd.read_excel('./../../data/prepared/prepared_ICU_Prediction.xlsx')
+
+        ground_truth = input_data['ICU']
+        sample_data = input_data.drop('ICU', axis=1)
+        train_data, test_data, train_truth, test_truth = train_test_split(sample_data, ground_truth, test_size=0.2, shuffle=True)
+
+        model = svm.LinearSVC()
+        metric_generator = ModelMetricsGenerator(test_truth)
+        metric_generator.generate_metrics(model, train_data, train_truth, test_data)
+        metric_generator.print_results()
+
+
+
+
+    if __name__ == '__main__':
+        unittest.main()
