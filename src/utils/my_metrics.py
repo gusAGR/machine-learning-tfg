c8030e6bd95f322fa32cf7341b2264d93d1f3553
Gustavo A <37037671+gusAGR@users.noreply.github.com>
2021-04-03 08:53:28 +0200

    project metrics

---
+++ src/utils/my_metrics.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import make_scorer, fbeta_score, recall_score
+
+
+def accuracy_precision_recall_specifity_f2_score():
+    f2_score = make_scorer(fbeta_score, beta=2)
+    specifity = make_scorer(recall_score, pos_label=0)
+    metrics = {'accuracy': 'accuracy', 'precision': 'precision', 'recall': 'recall', 'specifity': specifity, 'f2_score': f2_score }
+    return metrics
